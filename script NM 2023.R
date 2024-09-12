08616ff4a1248f6e985c38274b6c6c05fd9a5121
ja2905a <ja2905a@american.edu>
2024-09-11 22:08:34 -0400

    9/11

--- script NM 2023.R
+++ script NM 2023.R
@@ -21,7 +21,7 @@ NMPRCP <-
   mutate(LAB = case_when(
     PRCP > 0.32 ~ "NM's wettest day
     0.34 inches
-    Dec 23",
+    Dec 23", #label for graph
     PRCP < 0.32 ~ NA
   ))
 
@@ -34,17 +34,19 @@ ggplot(mapping = aes(x = DATE, y = PRCP)) +
   geom_text(
     aes(label = LAB),
     nudge_y = .05,
-    nudge_x = -5,
+    nudge_x = -20,
     size = 2.5,
     fontface = "bold"
   ) +
+  geom
   scale_y_continuous(
     limits = c(0, 0.5),
     expand = expansion(mult = c(0, 0.05))
   ) +
   scale_x_date(
     date_breaks = "2 month",
-    date_labels = c("Dec 1","Feb 1","Apr 1","Jun 1","Aug 1","Oct 1")
+    date_labels = c("Dec 1","Feb 1","Apr 1","Jun 1","Aug 1","Oct 1"),
+    expand = c(0,0)
   ) +
   labs(
     y = "Inches of Precipitation",
@@ -59,12 +61,31 @@ ggplot(mapping = aes(x = DATE, y = PRCP)) +
     axis.ticks.y = element_blank(),
     plot.title = element_text(hjust = .5),
     plot.subtitle = element_text(hjust = .5,
-                                 face = "bold")
+                                 face = "bold"),
+    axis.text = element_text(size = 10)
   )
   
+#avg and max temps by day, aggregate of stations' data
+NMTEMP <-
+  NM|>
+  select(DATE, TAVG, TMAX) |>
+  group_by(DATE) |>
+  mutate(TMAX = case_when(
+    TMAX > 115 ~ NA, # a few max temps over 200F
+    TMAX < 115 ~ TMAX
+  ))|>
+  summarize(across(c("TAVG", "TMAX"), ~ mean(.x, na.rm = TRUE)))
 
-mdpr x sn32
+NMTEMP |>
+ggplot(aes(x=DATE)) + 
+  geom_line(aes(y=TAVG, color="blue")) + 
+  geom_line(aes(y=TMAX, color="red"))
 
-tavg x tmax
+
+
+
+
+
+
 
 tmax x sx##
